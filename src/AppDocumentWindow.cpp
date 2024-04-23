cf8f44dff802d845f76274a3cbf121189a625e09
mrmike12333 <32998983+mrmike12333@users.noreply.github.com>
2024-04-23 20:54:52 +0100

    Made app window resizable

--- src/AppDocumentWindow.cpp
+++ src/AppDocumentWindow.cpp
@@ -7,6 +7,16 @@ AppDocumentWindow::AppDocumentWindow()
                 juce::DocumentWindow::TitleBarButtons::closeButton)
 {
     setUsingNativeTitleBar(true);
+    setResizable(true, false);
+
+    /*
+    *   Potentially hacky solution which may fail on multi-desktop.
+    *   TODO: Refactor for finding the display which the component is in.
+    */
+    const juce::Displays activeDisplays = juce::Desktop::getInstance().getDisplays();
+    const auto displayArea = activeDisplays.getPrimaryDisplay()->userArea;
+    setResizeLimits(400, 400,
+        displayArea.getWidth(), displayArea.getHeight());
 }
 
 void AppDocumentWindow::closeButtonPressed()
